e72b48654e887ea934d9adc9195fb3487703cdd0
thebigbadox <adamwebb13@gmail.com>
2018-08-27 20:01:58 -0500

    Completed Day 2 and starting code rework so I don't need to comment out each class after completetion

---
+++ AdventOfCode2017Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdventOfCode2017Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdventOfCode2017Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61762d74-ef7c-45a6-af24-881c56c93144")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AdventofCode2017/AdventDay2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode2017
+{
+    class AdventDay2
+    {
+
+        private static List<int[]> ReadFile()
+        {
+            List<int[]> returnArray = new List<int[]>();
+            int counter = 0;
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"e:\workspace\src\github.com\awebbdev\AdventofCode2017\AdventofCode2017\Day2.txt");
+            while((line = file.ReadLine()) != null)
+            {
+                string[] tmp = line.Split('\t');
+                int[] tmpArray = Array.ConvertAll(tmp, s => int.Parse(s));
+                returnArray.Add(tmpArray);
+            }
+            file.Close();
+            return returnArray;
+        }
+        private static List<int> GetChecks(List<int[]> input)
+        {
+            List<int> returnInts = new List<int>();
+            foreach(int[] line in input)
+            {
+                //returnInts.Add(line.Max() - line.Min());
+                for (int i = 0; i < line.Length; i++)
+                {
+                    for (int j = 0; j < line.Length; j++)
+                    {
+                        if (i == j)
+                        {
+                            continue;
+                        }
+                        else if (line[i] % line[j] == 0)
+                        {
+                            int k = line[i] / line[j];
+                            returnInts.Add(k);
+                            Console.WriteLine(k);
+                            break;
+                        }
+                    }
+                }
+            }
+            return returnInts;
+        }
+
+        private static int CalcCheckSum(List<int> input)
+        {
+            int i = 0;
+            foreach(int j in input)
+            {
+                i += j;
+            }
+            return i;
+        }
+    }
+}
